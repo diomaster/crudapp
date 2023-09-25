e14b9904717ac57ec0339cfc76041bef550493c2
dallensmith1@mail.weber.edu <Dallen Smith>
2023-09-25 12:13:27 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Main from './components/Main.js'
+import Layout from './components/Layout.js'
+import ProjectList from './components/ProjectList.js'
+import About from './components/about.js'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'; 
+
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/list" element={<ProjectList />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/" element={<Main />}></Route>
+      </Route>
+     </Routes>
     </div>
+    </Router>
   );
 }
 
